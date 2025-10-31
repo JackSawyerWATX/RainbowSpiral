320a9a5ed3b046d7c5f3f162fdeb69dee3522cfa
Jack Sawyer <contact@jonathanfausset.com>
2025-10-30 19:12:06 -0500

    first commit

---
+++ spiral.py
@@ -0,0 +1,13 @@
+import turtle
+
+t=turtle.Turtle(); t.speed(-15)
+
+colors=["red", "yellow", "green", "blue", "indigo", "violet"]
+
+for i in range(360):
+  t.pencolor(colors[i%6])
+  t.forward(i * 0.5)
+  t.right(61)
+  t.width(width=5)
+
+turtle.done()
