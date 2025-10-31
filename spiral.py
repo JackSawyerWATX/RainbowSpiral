5cbe86135ae264f7441f88c1b80dd5865aa63764
Jack Sawyer <contact@jonathanfausset.com>
2025-10-30 19:33:48 -0500

    Adjusting settings

--- spiral.py
+++ spiral.py
@@ -6,8 +6,8 @@ colors=["red", "yellow", "green", "blue", "indigo", "violet"]
 
 for i in range(360):
   t.pencolor(colors[i%6])
-  t.forward(i * 0.5)
-  t.right(61)
-  t.width(width=5)
+  t.forward(i * 1)
+  t.right(60.5)
+  t.width(width=25)
 
 turtle.done()
